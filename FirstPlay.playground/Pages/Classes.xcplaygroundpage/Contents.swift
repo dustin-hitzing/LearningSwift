8f73e4e467d8f2db5d03510044d15c70f052c4f2
Dustin Hitzing <dustinhitzing@Dustins-MacBook-Pro-3.local>
2016-03-07 18:49:10 -0500

    learned initializers in classes

--- FirstPlay.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ FirstPlay.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+
 class Shape {
     var numberOfSides: Int
     init(sides: Int){
@@ -7,3 +8,21 @@ class Shape {
 }
 
 let someShape = Shape(sides: 4)
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+}
+
+class Business {
+    var name: String
+    var location: Location
+    
+    init(name: String, lat: Double, long: Double){
+        self.location = Location(latitude: lat, longitude: long)
+        self.name = name
+        
+    }
+}
+
+let someBusiness = Business(name: "Bob", lat: 40.0, long: 50.0)
